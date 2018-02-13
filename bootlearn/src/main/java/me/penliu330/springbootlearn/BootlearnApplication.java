2f9ea37df54a162bb160e6c2e64701f541f15bb4
liupeng330 <330liupeng@gmail.com>
2018-02-14 00:17:17 +0800

    a

---
+++ bootlearn/src/main/java/me/penliu330/springbootlearn/BootlearnApplication.java
@@ -0,0 +1,12 @@
+package me.penliu330.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootlearnApplication.class, args);
+	}
+}
